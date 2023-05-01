7adb38233c0f3fe2befe0970bc150b77d2387fe1
MoeB <benazzouzamehdi@gmail.com>
2023-05-01 19:05:51 +0200

    [MBA] user crud 1

---
+++ src/main/java/com/amigoscode/livestockplatform/entity/UsersEntity.java
@@ -0,0 +1,170 @@
+package com.amigoscode.livestockplatform.entity;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "users", schema = "public", catalog = "postgres")
+public class UsersEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id")
+    private int id;
+    @Basic
+    @Column(name = "user_name")
+    private String userName;
+    @Basic
+    @Column(name = "first_name")
+    private String firstName;
+    @Basic
+    @Column(name = "last_name")
+    private String lastName;
+    @Basic
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @Basic
+    @Column(name = "street_name")
+    private String streetName;
+    @Basic
+    @Column(name = "postal_code")
+    private String postalCode;
+    @Basic
+    @Column(name = "city")
+    private String city;
+    @Basic
+    @Column(name = "state")
+    private String state;
+    @Basic
+    @Column(name = "country")
+    private String country;
+    @Basic
+    @Column(name = "email_address")
+    private String emailAddress;
+    @Basic
+    @Column(name = "delivery_address")
+    private String deliveryAddress;
+    @Basic
+    @Column(name = "billing_address")
+    private String billingAddress;
+    @Basic
+    @Column(name = "user_type")
+    private String userType;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getStreetName() {
+        return streetName;
+    }
+
+    public void setStreetName(String streetName) {
+        this.streetName = streetName;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public String getBillingAddress() {
+        return billingAddress;
+    }
+
+    public void setBillingAddress(String billingAddress) {
+        this.billingAddress = billingAddress;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsersEntity that = (UsersEntity) o;
+        return id == that.id && Objects.equals(firstName, that.firstName) && Objects.equals(lastName, that.lastName) && Objects.equals(phoneNumber, that.phoneNumber) && Objects.equals(streetName, that.streetName) && Objects.equals(postalCode, that.postalCode) && Objects.equals(city, that.city) && Objects.equals(state, that.state) && Objects.equals(country, that.country) && Objects.equals(emailAddress, that.emailAddress) && Objects.equals(deliveryAddress, that.deliveryAddress) && Objects.equals(billingAddress, that.billingAddress) && Objects.equals(userType, that.userType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, phoneNumber, streetName, postalCode, city, state, country, emailAddress, deliveryAddress, billingAddress, userType);
+    }
+}
---
+++ src/main/java/com/amigoscode/livestockplatform/service/UserService.java
@@ -0,0 +1,68 @@
+package com.amigoscode.livestockplatform.service;
+
+import com.amigoscode.livestockplatform.entity.UsersEntity;
+import com.amigoscode.livestockplatform.repository.UserDao;
+import com.amigoscodelivestock_platform.model.User;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public List<User> listUsers() {
+        List<UsersEntity> usersEntities = userDao.findAll();
+        return usersEntities.stream().map(this::buildUserModel).toList();
+    }
+
+    public Optional<User> getUser(int id) {
+        Optional<User> user = this.listUsers().stream().filter(u -> id == u.getId().intValue()).findFirst();
+        return user;
+    }
+
+
+    @Transactional
+    public User createUser(User user) {
+        Gson gson = new Gson();
+        String tmp = gson.toJson(user);
+        UsersEntity usersEntity = userDao.save(gson.fromJson(tmp, UsersEntity.class));
+        return buildUserModel(usersEntity);
+    }
+
+    @Transactional
+    public User updateUser(Integer id, User user) {
+        user.setId(BigDecimal.valueOf(id));
+        Gson gson = new Gson();
+        String tmp = gson.toJson(user);
+
+        UsersEntity usersEntity = userDao.save(gson.fromJson(tmp, UsersEntity.class));
+
+        return buildUserModel(usersEntity);
+    }
+
+    public void deleteUser(Integer id) {
+        userDao.deleteById(id);
+    }
+
+
+
+    private User buildUserModel(UsersEntity usersEntity) {
+        Gson gson = new Gson();
+        String tmp = gson.toJson(usersEntity);
+        return gson.fromJson(tmp, User.class);
+    }
+
+}
