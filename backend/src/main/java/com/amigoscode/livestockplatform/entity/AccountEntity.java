2762d0d5ca2e7499748d463c795a784f40ddd39e
MoeB <benazzouzamehdi@gmail.com>
2023-05-24 21:39:04 +0200

    [MBA] Users can sign up to platform via a sign up page choosing their role in the process

---
+++ backend/src/main/java/com/amigoscode/livestockplatform/entity/AccountEntity.java
@@ -0,0 +1,117 @@
+package com.amigoscode.livestockplatform.entity;
+
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "account", schema = "public", catalog = "postgres")
+public class AccountEntity {
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "id", nullable = false)
+    private int id;
+    @Basic
+    @Column(name = "username", nullable = false, length = 50)
+    private String username;
+    @Basic
+    @Column(name = "password", nullable = false, length = 500)
+    private String password;
+    @Basic
+    @Column(name = "email", nullable = true, length = 50)
+    private String email;
+    @Basic
+    @Column(name = "role", nullable = false, length = 50)
+    private String role;
+    @Basic
+    @Column(name = "user_id", nullable = false)
+    private int userId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+
+        AccountEntity that = (AccountEntity) o;
+
+        if (id != that.id)
+            return false;
+        if (userId != that.userId)
+            return false;
+        if (username != null ? !username.equals(that.username) : that.username != null)
+            return false;
+        if (password != null ? !password.equals(that.password) : that.password != null)
+            return false;
+        if (email != null ? !email.equals(that.email) : that.email != null)
+            return false;
+        if (role != null ? !role.equals(that.role) : that.role != null)
+            return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        result = 31 * result + (role != null ? role.hashCode() : 0);
+        result = 31 * result + userId;
+        return result;
+    }
+}
